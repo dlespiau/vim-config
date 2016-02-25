977b7430108b6f8a3e5f24b85d7171c8fe91fd2a
Michael Wood <michael.g.wood@intel.com>
2016-02-25 16:53:06 +0000

    install Use the post install hooks

--- install.sh
+++ install.sh
@@ -1,19 +1,10 @@
 #!/bin/sh
 
-
 mv ~/.vimrc ~/.vimrc-backup
 mv ~/.vim ~/.vim-backup
 
 cp -r ./ ~/.vim/
 
-YOU_COMPLETE_INSTALLED=`dpkg-query -f='${db:Status-Status}' -W vim-youcompleteme`
-
-if [ "$YOU_COMPLETE_INSTALLED" != "installed" ]
-then
-	echo "Installing vim-youcompleteme"
-	sudo apt-get install vim-youcompleteme
-fi
-
-vam install youcompleteme
+sudo apt-get install ruby-dev build-essential
 
 vim -c PlugInstall
