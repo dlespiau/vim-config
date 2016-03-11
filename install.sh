9df364170ed5fd1da0b328fb0458e5c1db3e07d6
Michael Wood <michael.g.wood@intel.com>
2016-03-11 11:11:08 +0000

    Add python-dev needed for compiling YouCompleteMe
    Signed-off-by: Michael Wood <michael.g.wood@intel.com>

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ mv ~/.vim ~/.vim-backup
 
 cp -r ./ ~/.vim/
 
-sudo apt-get install ruby-dev build-essential cmake
+sudo apt-get install ruby-dev build-essential cmake python-dev
 
 vim -c PlugInstall
